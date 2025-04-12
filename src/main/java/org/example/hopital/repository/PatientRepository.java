9698518f46f5f2cd48489ed47a807cd639b3f044
LAPTOP-GQA9U98Q\hamza <Hamza.Tabat@emsi-edu.ma>
2025-04-12 17:18:08 +0100

    first commit

---
+++ src/main/java/org/example/hopital/repository/PatientRepository.java
@@ -0,0 +1,5 @@
+package org.example.hopital.repository;
+import  org.example.hopital.entities.Patient;
+import  org.springframework.data.jpa.repository.JpaRepository;
+public interface PatientRepository extends JpaRepository<Patient,Long> {
+}
